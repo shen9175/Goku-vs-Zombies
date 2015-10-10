e7b95ba2d5e118ef05564e7f26362d41c88e3ab4
shen9175 <billsxz@gmail.com>
2015-10-09 20:08:55 -0700

    Update Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -18,9 +18,6 @@ namespace XNAtest
         public bool ocupied;
     };
 
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
@@ -59,16 +56,8 @@ namespace XNAtest
             graphics.ApplyChanges();
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            //position = new Vector2(400, 300);
             rand=new Random();
             missle = new LinkedList<wave>();
             missle.Clear();
@@ -174,10 +163,7 @@ namespace XNAtest
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -191,24 +177,14 @@ namespace XNAtest
             font = Content.Load<SpriteFont>("SpriteFont1");
             IsMouseVisible = true;
 
-
-
-            // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
+
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
